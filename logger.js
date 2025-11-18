1bed0936d680f81e943621ddc431b4b63ee54014
dummy422 <dummywebsite42@gmail.com>
2025-11-18 10:42:34 +0530

    Create logger.js

---
+++ logger.js
@@ -0,0 +1,12 @@
+// logger.js
+const winston = require('winston');
+const level = process.env.LOG_LEVEL || 'info';
+const logger = winston.createLogger({
+  level,
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.printf(({ timestamp, level, message }) => `${timestamp} ${level}: ${message}`)
+  ),
+  transports: [new winston.transports.Console()]
+});
+module.exports = logger;
